8b45caa78d70f7648f6721d2232af79c5f83dfe7
Aditi Bhosure <bhosure.as2003@gmail.com>
2026-01-11 22:57:14 +0530

    Week 2 Task: Weather App with API integration

---
+++ Week 3/login_signup_app/lib/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          )
+        ],
+      ),
+      body: const Center(
+        child: Text(
+          "Welcome! 🎉",
+          style: TextStyle(fontSize: 22),
+        ),
+      ),
+    );
+  }
+}
